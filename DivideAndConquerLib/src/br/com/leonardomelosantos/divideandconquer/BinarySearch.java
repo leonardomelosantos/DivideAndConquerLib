d055cb5e17ff5c0eab253ced626ce8623c7f4a82
leonardomelosantos <leonardomelosantos@gmail.com>
2013-02-19 13:29:59 -0300

    First commit.

---
+++ DivideAndConquerLib/src/br/com/leonardomelosantos/divideandconquer/BinarySearch.java
@@ -0,0 +1,42 @@
+package br.com.leonardomelosantos.divideandconquer;
+
+public class BinarySearch {
+
+	/**
+	 * Return the position of the number.
+	 * 
+	 * @param arrayContent
+	 * @param numberToFind
+	 * @return
+	 */
+	public static int searchBinary(int[] arrayContent, int numberToFind) {
+		
+		return searchBinary(arrayContent, numberToFind, 1, arrayContent.length);
+	}
+	
+	private static int searchBinary(int[] arrayContent, int numberToFind, int initialPosition, int finalPosition)
+	{
+		int returnValue = 0;
+		int middleElement = (initialPosition + finalPosition) / 2;
+		int middleElementZeroBazed = middleElement--;
+		
+		if (initialPosition > finalPosition)
+		{
+			returnValue = 0;
+		} else {
+			if (numberToFind == arrayContent[middleElementZeroBazed]) {
+	            returnValue = middleElementZeroBazed;
+	        } else {
+	            if (numberToFind < arrayContent[middleElementZeroBazed]) {
+	            	returnValue = searchBinary(arrayContent, numberToFind, initialPosition, middleElementZeroBazed-1);
+	            } else {
+	            	returnValue = searchBinary(arrayContent, numberToFind, middleElementZeroBazed+1, finalPosition);
+	            }
+	        }
+		}
+		
+		
+        return returnValue;
+	}
+	
+}
---
+++ DivideAndConquerLib/src/br/com/leonardomelosantos/divideandconquer/MergeSort.java
@@ -0,0 +1,72 @@
+package br.com.leonardomelosantos.divideandconquer;
+
+public class MergeSort {
+	
+	public static int[] mergeSort(int[] vetorDesordenado) {
+		return mergeSort(vetorDesordenado, vetorDesordenado.length-1);
+	}
+	
+	private static int[] mergeSort(int[] arrayUnsorted, int x)
+	{
+		int[] returnValue;
+		
+		if (x == 0)
+		{
+			returnValue = arrayUnsorted;
+		}
+		else
+		{
+			int nEsq = x / 2;
+			int nDir = nEsq + 1;
+
+			int[] VetorEsq = QVetor(arrayUnsorted, 0, nEsq);
+			int[] VetorDir = QVetor(arrayUnsorted, nDir, x);
+
+			int[] VetorU_DeCombina = mergeSort(VetorEsq, nEsq);
+			int[] VetorV_DeCombina = mergeSort(VetorDir, x-nDir);
+
+			returnValue = Combina(VetorU_DeCombina, nEsq, VetorV_DeCombina, x-nDir);
+		}
+	
+		return returnValue;
+	}
+	
+	private static int[] QVetor(int[] ParVet, int inicio, int fim)
+	{
+		int[] VetorAux = new int[ParVet.length];
+
+		int nn = 0;
+		for (int i = inicio; i<=fim; i++) {
+			VetorAux[nn] = ParVet[i];
+			nn++;
+		}
+		
+		return VetorAux;
+	}
+	
+	private static int[] Combina(int[] VetorU, int nnn, int[] VetorV, int mmm)
+	{
+		int i, j, w;
+		int[] VetorW = new int[VetorU.length];
+
+		VetorU[nnn + 1] = Integer.MAX_VALUE;
+		VetorV[mmm + 1] = Integer.MAX_VALUE;
+		w = 0;
+		i = 0;
+		j = 0;
+	
+		while (i <= nnn || j <= mmm) {
+			if (VetorU[i] <= VetorV[j]) {
+				VetorW[w] = VetorU[i];
+				i++;
+			} else {
+				VetorW[w] = VetorV[j];
+				j++;
+			}
+			w++;
+		}
+		
+		return VetorW;
+	}
+	
+}
